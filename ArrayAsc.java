a6297a5dc138cd1b11f355784c09f3ac76eff642
Gauri2230 <108929783+Gauri2230@users.noreply.github.com>
2022-08-04 15:46:04 +0530

    Add files via upload

---
+++ ArrayAsc.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class ArrayAsc
+{
+	public static void main(String args[])
+	{
+		int temp=0;
+		int ex[]= new int[5];
+		Scanner s = new Scanner(System.in);
+		for (int i=0;i<ex.length;i++)
+		{
+			ex[i]=s.nextInt();
+		}
+		for (int i = 0; i < ex.length; i++) 
+		{     
+		            for (int j = i+1; j < ex.length; j++) 
+			{     
+			               if(ex[i] >ex[j]) 
+				{    
+                 		           		temp = ex[i];    
+                   				ex[i] = ex[j];    
+                   				ex[j] = temp; 
+				}
+			}
+			System.out.println(ex[i] );   
+		}
+			
+	} 
+}
